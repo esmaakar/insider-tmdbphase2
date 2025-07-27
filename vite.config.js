b98e10e5efa51d2497e96b1238f3b6d302a7fdb5
esmaakar <esmaakar171@gmail.com>
2025-07-27 19:20:17 +0300

    vite.config.js base ayarı güncellendi

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/tmdbphase2/',
+  base: '/insider-tmdbphase2/',
   plugins: [react()]
 })
